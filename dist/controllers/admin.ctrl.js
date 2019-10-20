33d6a352f7480f83f64f24f6553eb8dcf510540b
Tuan Khang <tuankhang@Tuans-MacBook-Pro.local>
2019-10-20 12:32:00 +0700

    OKKKk

--- dist/controllers/admin.ctrl.js
+++ dist/controllers/admin.ctrl.js
@@ -8,7 +8,7 @@ const os = require('os')
 const os_uil = require('os-utils')
 const config = require('./../settings/configs')
 const crypto = require('crypto')
-let GetHash=(input)=>{
+let GetHash = (input) => {
     return crypto.createHash('sha1').update(input).digest('hex')
 }
 module.exports = {
@@ -33,122 +33,131 @@ module.exports = {
         let user = req.body
         console.log(user)
         let setuser = {
-               username:'admin',
-               password: GetHash('n0p4ssw0rd!!'),
-               info:'5d7f9d80c3874a085d4fe842'
+            username: 'admin',
+            password: GetHash('n0p4ssw0rd!!'),
+            info: '5d7f9d80c3874a085d4fe842'
         }
-        Admin(setuser).save((err,data)=>{
+        Admin(setuser).save((err, data) => {
             console.log(data)
         })
         console.log(GetHash(user.userId));
-        if(user.userId){
-            Admin.findOne({username:user.userId})
-            .populate('info')
-            .exec((err,resp)=>{
-                if(!err){
-                    if(GetHash(user.password)===resp.password){
-                        let data = resp.info;
-                        let response = {
-                            email: data.email,
-                            followers: data.followers,
-                            following: data.following,
-                            name: data.name,
-                            post_permission: data.post_permission,
-                            point: data.point,
-                            provider: data.provider,
-                            provider_id: data.provider_id,
-                            provider_pic: data.provider_pic,
-                            token: data.token,
-                            _id: data._id,
-                            jwtToken: MiddleWare.genTokenAd(resp.info._id,'a12m1n')
+        if (user.userId) {
+            user.userId = user.userId.replace('"', '');
+            user.password = user.password.replace('"', '');
+            Admin.findOne({
+                    username: user.userId
+                })
+                .populate('info')
+                .exec((err, resp) => {
+                    if (!err && resp) {
+                        if (GetHash(user.password) === resp.password) {
+                            let data = resp.info;
+                            let response = {
+                                email: data.email,
+                                followers: data.followers,
+                                following: data.following,
+                                name: data.name,
+                                post_permission: data.post_permission,
+                                point: data.point,
+                                provider: data.provider,
+                                provider_id: data.provider_id,
+                                provider_pic: data.provider_pic,
+                                token: data.token,
+                                _id: data._id,
+                                jwtToken: MiddleWare.genTokenAd(resp.info._id, 'a12m1n')
+                            }
+                            res.json(response)
+                            console.log(response);
+                        } else {
+                            res.status(401).send('Something Was Wrong!!')
                         }
-                        res.json(response)
-                        console.log(response);
-                    }
-                    else{
+
+                    } else {
                         res.status(401).send('Something Was Wrong!!')
                     }
-                   
-                }
-                else{
-                    res.status(401).send('Something Was Wrong!!')
-                }  
-            })
-            }
-            else{
-                res.status(401).send('Something Was Wrong!!')
-            }  
-        
+                })
+        } else {
+            res.status(401).send('Something Was Wrong!!')
+        }
+
     },
-    loginApp: (req,res)=>{
+    loginApp: (req, res) => {
         console.log(req.body)
         let account = {
-            user:'admin',
-            password:'password'
+            user: 'admin',
+            password: 'password'
         }
-        if(req.body.user === account.user && req.body.password === account.password){
+        if (req.body.user === account.user && req.body.password === account.password) {
             console.log(req.body)
             let resp = {
-                status:'Success',
+                status: 'Success',
                 access_token: MiddleWare.genToken('admin').access_token
             }
 
             res.json(resp)
+        } else {
+            res.json({
+                status: 'Unauthorized',
+                access_token: 'not-alow'
+            })
         }
-        else{
-            res.json({status:'Unauthorized',access_token:'not-alow'})
-        }
-        
+
     },
-    getAllUser:  (req,res)=> {
-        User.find({},{name:1,email:1,point:1,provider_pic:1,post_permission:1}).then((result)=>{
+    getAllUser: (req, res) => {
+        User.find({}, {
+            name: 1,
+            email: 1,
+            point: 1,
+            provider_pic: 1,
+            post_permission: 1
+        }).then((result) => {
             console.log(result)
             res.json(result)
-           
+
         })
     },
-    ramMonitor: (req,res)=>{
+    ramMonitor: (req, res) => {
         let ram = {
-            total: os.totalmem()/(1024),
-            free: os.freemem()/(1024),
+            total: os.totalmem() / (1024),
+            free: os.freemem() / (1024),
         }
-        
-     res.json(ram)
+
+        res.json(ram)
         console.log(ram)
     },
-    cpuMonitor: (req,res)=>{
-        
-        os_uil.cpuUsage(v=>{
-            
-          info.networkStats().then(data=>{
-           let netrx= data[0].rx_sec/(8*1024);
-           let nettx= data[0].tx_sec/(8*1024);
-           if(netrx<=0){
-               netrx=0;
-           }
-           if(nettx<=0){
-            nettx=0;
-        }
-           if(netrx>100){
-               netrx=100
-           }
-           if(nettx>100){
-            nettx=100
-        }
-              
-            let cpu= {
-                cpu: v * 100,
-                ram: 100-((os.freemem()/os.totalmem()) * 100),
-                netrx:netrx,
-                nettx:nettx
-                
-            }
-            res.json(cpu)
-        })
-            
+    cpuMonitor: (req, res) => {
+
+        os_uil.cpuUsage(v => {
+
+            info.networkStats().then(data => {
+                let netrx = data[0].rx_sec / (8 * 1024);
+                let nettx = data[0].tx_sec / (8 * 1024);
+                if (netrx <= 0) {
+                    netrx = 0;
+                }
+                if (nettx <= 0) {
+                    nettx = 0;
+                }
+                if (netrx > 100) {
+                    netrx = 100
+                }
+                if (nettx > 100) {
+                    nettx = 100
+                }
+
+                let cpu = {
+                    cpu: v * 100,
+                    ram: 100 - ((os.freemem() / os.totalmem()) * 100),
+                    netrx: netrx,
+                    nettx: nettx
+
+                }
+                res.json(cpu)
+            })
+
         })
     },
-    setData: (req,res)=>{
+    setData: (req, res) => {
         new Article(config.Data).save((err, article) => {
             if (err)
                 res.send(err)
@@ -162,30 +171,29 @@ module.exports = {
             next()
         })
     },
-    getAllUsers:  (req,res)=> {
+    getAllUsers: (req, res) => {
         let token = req.body.token
         console.log(MiddleWare.withAuthAd(token))
         let isAdmin = MiddleWare.withAuthAd(token)
-        if(isAdmin){
-            User.find({}).then((result)=>{
+        if (isAdmin) {
+            User.find({}).then((result) => {
                 // console.log(result)
-                 res.json(result)
-             })
-        }
-        else{
+                res.json(result)
+            })
+        } else {
             res.status(401).send('Sometthing Was Wrong!!')
         }
-        
+
     },
-    setUser:(req,res,next)=>{
-            User(config.Accounts).save((err,mem)=>{
-                if (err)
+    setUser: (req, res, next) => {
+        User(config.Accounts).save((err, mem) => {
+            if (err)
                 res.send(err)
             else if (!mem)
                 res.send(400)
-                else res.send(mem)
-            })
-           
+            else res.send(mem)
+        })
+
     }
-    
+
 }
